b3e56782d7dd4227a73a4a76aaf97429cfb1fef5
Trigah <ijenrandy2@gmail.com>
2023-06-06 02:05:17 +0100

    Stock News Alert Project

--- day-35/rain-alert/main.py
+++ day-35/rain-alert/main.py
@@ -5,15 +5,17 @@ import requests
 import os
 from twilio.rest import Client
 from twilio.http.http_client import TwilioHttpClient
+from dotenv import load_dotenv,dotenv_values
 
-OWM_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
+
+OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
 api_key = os.environ.get("OWM_API_KEY")
-account_sid = "YOUR ACCOUNT SID"
+account_sid = "AC85e7b370a505c56541ddd89934c6056f"
 auth_token = os.environ.get("AUTH_TOKEN")
 
 weather_params = {
-    "lat": "YOUR LATITUDE",
-    "lon": "YOUR LONGITUDE",
+    "lat": "9.044598",
+    "lon": "7.486096",
     "appid": api_key,
     "exclude": "current,minutely,daily"
 }
@@ -32,13 +34,13 @@ for hour_data in weather_slice:
 
 if will_rain:
     proxy_client = TwilioHttpClient()
-    proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+    # proxy_client.session.proxies = {'https': os.environ['https_proxy']}
 
     client = Client(account_sid, auth_token, http_client=proxy_client)
     message = client.messages \
         .create(
         body="It's going to rain today. Remember to bring an ☔️",
-        from_="YOUR TWILIO VIRTUAL NUMBER",
-        to="YOUR TWILIO VERIFIED REAL NUMBER"
+        from_="+13613091765",
+        to="+2348172829491"
     )
     print(message.status)
---
+++ day-36/stock-news-project/main.py
@@ -0,0 +1,89 @@
+import requests
+import os
+from datetime import date,timedelta
+from twilio.rest import Client
+from twilio.http.http_client import TwilioHttpClient
+STOCK = "TSLA"
+COMPANY_NAME = "Tesla Inc"
+
+STOCK_ENDPOINT ="https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = os.environ.get("STOCK_API_KEY")
+NEWS_API_KEY = os.environ.get("NEWS_API_KEY")
+
+account_sid = "AC85e7b370a505c56541ddd89934c6056f"
+auth_token = os.environ.get("AUTH_TOKEN")
+print(auth_token)
+
+## STEP 1: Use https://www.alphavantage.co
+# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+stock_parameters = {
+    "function":"TIME_SERIES_DAILY_ADJUSTED",
+    "symbol":STOCK,
+    "apikey":STOCK_API_KEY
+}
+response = requests.get(STOCK_ENDPOINT, params=stock_parameters)
+stock_data = response.json()["Time Series (Daily)"]
+
+stock_data_dates=[key for (key,value) in stock_data.items()]
+yesterday_date = stock_data_dates[0]
+day_before_yesterday_date = stock_data_dates[1]
+
+yesterday_stock_close_price=stock_data[yesterday_date]["4. close"]
+day_before_yesterday_stock_close_price=stock_data[day_before_yesterday_date]["4. close"]
+
+print(yesterday_stock_close_price)
+print(day_before_yesterday_stock_close_price)
+
+def get_percentage_increase(num_a, num_b):
+    return round(((num_a - num_b) / num_b) * 100)
+
+percent=get_percentage_increase(float(yesterday_stock_close_price), float(day_before_yesterday_stock_close_price))
+
+## STEP 2: Use https://newsapi.org
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+
+news_parameters = {
+    "q":"tesla",
+    "from":stock_data_dates[1],
+    "sortBy":"publishedAt",
+    "apiKey":NEWS_API_KEY
+}
+response = requests.get(NEWS_ENDPOINT,params=news_parameters)
+stock_news_data = response.json()["articles"][0]
+
+title=stock_news_data["title"]
+description=stock_news_data["description"]
+
+## STEP 3: Use https://www.twilio.com
+# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+
+proxy_client = TwilioHttpClient()
+# proxy_client.session.proxies = {'https': os.environ['https_proxy']}
+
+client = Client(account_sid, auth_token, http_client=proxy_client)
+message = client.messages \
+    .create(
+    body=
+    
+    f"""
+    {STOCK}: 🔺{percent}%
+    Headline: Were Hedge Funds Right About Piling Into {COMPANY_NAME}. ({STOCK})?. 
+    Brief: {description}.
+    """,
+    from_="+13613091765",
+    to="+2348172829491"
+)
+print(message.status)
+#Optional: Format the SMS message like this: 
+"""
+TSLA: 🔺2%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+or
+"TSLA: 🔻5%
+Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
+Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
+"""
+
